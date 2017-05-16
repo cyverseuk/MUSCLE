dd52e22b6aa3c2f19886b115437be57e1de7652a
aliceminotto <i.m.e@hotmail.it>
2017-05-16 14:22:27 +0100

    add wrapper script

---
+++ wrapper/wrapper.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function debug {
+  echo "creating debugging directory"
+mkdir .debug
+for word in ${rmthis}
+  do
+    if [[ "${word}" == *.sh ]] || [[ "${word}" == lib ]]
+      then
+        mv "${word}" .debug;
+      fi
+  done
+}
+
+rmthis=`ls`
+echo ${rmthis}
+
+ARGSU=" ${diags} ${format} "
+INPUTSU="${input_file}"
+echo "Input files is " "${INPUTSU}"
+echo "Arguments are " "${ARGSU}"
+
+CMDLINEARG="muscle ${ARGSU} -in ${INPUTSU} -out muscle_output -log muscle_log "
+
+echo ${CMDLINEARG};
+
+chmod +x launch.sh
+
+echo  universe                = docker >> lib/condorSubmitEdit.htc
+echo docker_image            =  cyverseuk/muscle:v3.8.31 >> lib/condorSubmitEdit.htc ######
+echo executable               =  ./launch.sh >> lib/condorSubmitEdit.htc #####
+echo arguments                          = ${CMDLINEARG} >> lib/condorSubmitEdit.htc
+echo transfer_input_files = ${INPUTSU}, launch.sh >> lib/condorSubmitEdit.htc
+#echo transfer_output_files = output >> lib/condorSubmitEdit.htc
+cat /mnt/data/apps/muscle/lib/condorSubmit.htc >> lib/condorSubmitEdit.htc
+
+less lib/condorSubmitEdit.htc
+
+jobid=`condor_submit -batch-name ${PWD##*/} lib/condorSubmitEdit.htc`
+jobid=`echo $jobid | sed -e 's/Sub.*uster //'`
+jobid=`echo $jobid | sed -e 's/\.//'`
+
+#echo $jobid
+
+#echo going to monitor job $jobid
+condor_tail -f $jobid
+
+debug
+
+exit 0
